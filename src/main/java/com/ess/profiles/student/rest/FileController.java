a57bd192725649da2b0a206d12ccfa68cda43950
ndjana30 <davyndjana@gmail.com>
2024-07-31 10:26:01 +0200

    new commit

--- src/main/java/com/ess/profiles/student/rest/FileController.java
+++ src/main/java/com/ess/profiles/student/rest/FileController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://profileswebess.onrender.com/")
 @RestController
 @RequestMapping("api/v1/files")
 // @CrossOrigin("*")
